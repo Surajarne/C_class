355a90559cd7ad5fc13107b19a50b32b6eaabf11
Your Name <you@example.com>
2024-11-19 09:21:16 +0530

    first commit

---
+++ pov.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int  main(){
+	int a;
+	printf("enter the num :");
+	scanf("%d",&a);
+
+ if(a>=0)
+{	printf("the num is positive \n");
+}
+  else
+
+{       printf("the num is negavtive \n");
+}
+      return 0;		
+}
